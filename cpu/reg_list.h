814f2b13f5c61be21add8ab3787fa5aac2bebcc3
Eric Konks <43612097+exucutional@users.noreply.github.com>
2018-11-06 04:35:02 +0300

    ver.1.0.0

--- cpu/reg_list.h
+++ cpu/reg_list.h
@@ -1,8 +1,15 @@
+/*!
+ * \brief reg_list.h
+ * \author Exdev
+ * \version 1.0.0
+ * \brief Format : DEF_REG(name)
+ */
+
 DEF_REG(RAX)
 DEF_REG(RCX)
 DEF_REG(RDX)
-DEF_REF(RBX)
+DEF_REG(RBX)
 DEF_REG(RSP)
 DEF_REG(RBP)
 DEF_REG(RSI)
-DEF_REF(RDI)
+DEF_REG(RDI)
