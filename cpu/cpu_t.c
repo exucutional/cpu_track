d52815a9b71d5f7a167a2b55f97211dce9680882
Eric Konks <43612097+exucutional@users.noreply.github.com>
2018-11-06 04:36:34 +0300

    ver.6.0.0

--- cpu/cpu_t.c
+++ cpu/cpu_t.c
@@ -1,60 +1,113 @@
+/*!
+ * \brief cpu_t.c
+ * \author Exdev
+ * \version 0.6.0
+ */
+
 #include "cpu_t.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
 
-
-
-
-
-
-
-
-
-
-
-
+/*
 
 // Shell for amd64-like stack
 // add check later (trap segfault)
+
+/*
 inline void cpu_push(struct cpu_t *cpu, uint64_t val)
 {
 	cpu->reg[REG_RSP] -= sizeof(uint64_t);
-	*(uint64_t) (cpu->reg[REG_RSP]) = val;
+	*(uint64_t*) (cpu->reg[REG_RSP]) = val;
 }
+
 inline uint64_t cpu_pop(struct cpu_t *cpu)
 {
-	register int val = *(uint64_t) (cpu->reg[REG_RSP]);
+	register int val = *(uint64_t*) (cpu->reg[REG_RSP]);
 	(cpu->reg[REG_RSP]) += sizeof(uint64_t);
+	return val;
 }
 
-// Supported syscalls
-// RCX - syscall number 
-// 1 - input qword in rax
-// 2 - out qword from rax
-// 3 - input double in rax
-// 4 - out double from rax
-inline int cpu_syscall(struct cpu_t *cpu)
-{
-	switch (cpu->reg[REG_RCX]) {
-	case 3:
-		scanf("%lg", reg[REG_RAX]);
-		return 0;
-	default:
-		cpu->trap = TRAP_WRONG_SYSCALL;
-	}
+/// CPU commands:
+static inline int cpu_cmd_pushm(struct cpu_t *cpu) {return 0;}
+static inline int cpu_cmd_pushmr(struct cpu_t *cpu) {return 0;}
+static inline int cpu_cmd_popr(struct cpu_t *cpu) {return 0;}
+static inline int cpu_cmd_popm(struct cpu_t *cpu) {return 0;}
+static inline int cpu_cmd_popmr(struct cpu_t *cpu) {return 0;}
+static inline int cpu_cmd_add(struct cpu_t *cpu) {return 0;}
+static inline int cpu_cmd_sub(struct cpu_t *cpu) {return 0;}
+static inline int cpu_cmd_mul(struct cpu_t *cpu) {return 0;}
+static inline int cpu_cmd_div(struct cpu_t *cpu) {return 0;}
 
-
-/// CPU comands:
 inline int cpu_cmd_pushq(struct cpu_t *cpu)
 {
-	cpu_push(cpu, *(uint64_t) cpu->rip);
+	stack_push(&cpu->cpu_stk, (uint64_t)cpu->rip);
 	cpu->rip += sizeof(uint64_t);
+	return 0;
+}
+
+inline int cpu_cmd_pushr(struct cpu_t *cpu)
+{
+	uint8_t reg = *(uint8_t*) cpu->rip;
+	cpu->rip += sizeof(uint8_t);
+	stack_push(&cpu->cpu_stk, (uint64_t)cpu->reg[reg]);
+	return 0;
 }
 
 inline int cpu_cmd_nop(struct cpu_t *cpu)
 {
 	// nop
+	return 0;
 }
 
 inline int cpu_cmd_syscall(struct cpu_t *cpu)
 {
 	cpu->trap = TRAP_SYSCALL;
+	return 0;
 }
+
+int cpu_syscall(struct cpu_t *cpu)
+{
+	switch(cpu->reg[REG_RAX]) {
+	case SYSCALL_EXIT:
+		cpu->trap = TRAP_EXIT;
+		break;
+	default:
+		cpu->trap = TRAP_WRONG_SYSCALL;
+		return -1;
+	}
+	return 0;
+}
+
+int cpu_exec(struct cpu_t *cpu)
+{
+	while (1) {
+		if (!cpu->trap) {
+			if (cpu->trap == TRAP_SYSCALL)
+				cpu_syscall(cpu);
+			if (cpu->trap == TRAP_EXIT)
+				return 0;
+			if (cpu->trap == TRAP_WRONG_SYSCALL)
+				return -1;
+		}
+
+		uint8_t cmd = *(uint8_t*) (cpu->rip);
+		(cpu->rip)++;
+		switch (cmd) {
+
+#define DEF_CMD(name)		\
+		case CMD_##name:		\
+			cpu_cmd_##name(cpu);\
+			break;
+
+#include "cmd_list.h"
+#undef DEF_CMD
+
+		default:
+			printf("Wrong cmd\n");
+			return -1;
+		}
+	}
+}
+*/
--- cpu/cpu_t.h
+++ cpu/cpu_t.h
@@ -1,39 +1,79 @@
-#ifndef _CPU_T_H
+/*!
+ * \brief cpu_t.h
+ * \author Exdev
+ * \version 0.6.0
+ */
+
+#ifndef _CPU_T_H_
 #define _CPU_T_H_
 
-enum CMD_CODES {
-#define DEF_CMD(name)	CMD_##name,
-#include "cmd_list.h"
-CMD_MAX,
-#undef DEF_CMD
+#include <stdint.h>
+#include "stack_t.h"
+#include <string.h>
+
+#define LABEL_MAX 1024
+
+enum CMD_CODES
+{
+	#define DEF_CMD(name, val)	CMD_##name,
+	#include "cmd_list.h"
+	CMD_MAX,
+	#undef DEF_CMD
 };
 
-enum REG_CODES {
-#define DEF_REG(name)	REG_##name,
-#include "reg_list.h"
-REG_MAX,
-#undef DEF_REG
+enum REG_CODES
+{
+	#define DEF_REG(name)	REG_##name,
+	#include "reg_list.h"
+	REG_MAX,
+	#undef DEF_REG
 };
 
-enum TRAP_CODES {
-TRAP_NO_TRAP,
-TRAP_SYSCALL,
-TRAP_WRONG_SYSCALL,
-TRAP_MEMORY_FAIL,
+enum TRAP_CODES
+{
+	TRAP_NO_TRAP,
+	TRAP_EXIT,
+	TRAP_SYSCALL,
+	TRAP_WRONG_SYSCALL,
+	TRAP_MEMORY_FAIL,
 };
 
+enum SYSCALL_CODES
+{
+	SYSCALL_EXIT,
+	SYSCALL_INPQ,
+};
 
-struct cpu_t {
-	uint64_t rip;
+struct label_t
+{
+	size_t offs;
+	int isdefined;
+}lbl[LABEL_MAX];
+
+struct cpu_t
+{
+	uint64_t *rip;
 	uint64_t reg[REG_MAX];
+	struct stack cpu_stk;
 	uint64_t trap;
 };
 
 ssize_t code_asm (char    *text, uint8_t **code_p);
-ssize_t code_dasm(uint8_t *code, char    **text_p);
+ssize_t code_dasm(uint8_t *code, char **text_p, ssize_t size);
+
+int get_cmd(char **text_p, uint8_t **code_p);
+int get_reg(char **text_p, uint8_t **code_p);
+int get_num(char **text_p, uint8_t **code_p);
+int get_label(char **text_p, uint8_t **code_p);
+
+int put_cmd(char **text_p, uint8_t **code_p);
+int put_reg(char **text_p, uint8_t **code_p);
+int put_num(char **text_p, uint8_t **code_p);
+int put_label(char **text_p, uint8_t **code_p);
 
 int	cpu_init(struct cpu_t *cpu);
 int	cpu_load(struct cpu_t *cpu, uint8_t *code);
 int	cpu_exec(struct cpu_t *cpu);
+
 
 #endif // _CPU_T_H_
