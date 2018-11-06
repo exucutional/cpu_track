85d8da33933081c503e42802a7204b8f27d0e757
Eric Konks <43612097+exucutional@users.noreply.github.com>
2018-11-06 04:34:20 +0300

    ver.1.0.0

--- cpu/cmd_list.h
+++ cpu/cmd_list.h
@@ -1,14 +1,26 @@
-// do nothing
-DEF_CMD(nop)
-// syscall
-DEF_CMD(syscall)
+/*!
+ * \brief cmd_list.h
+ * \author Exdev
+ * \version 1.0.0
+ * \brief Format : DEF_CMD(name, value)
+ * value = 1 -> next act = get cmd
+ * value = 2 -> next act = get reg
+ * value = 3 -> next act = get number
+ * value = 4 -> next act = get label
+ */
 
-// push qword
-DEF_CMD(pushq)
-// push reg
-DEF_CMD(pushr)
-// pop to reg
-DEF_CMD(popr)
 
-// add two doubles from stack, push value to stack
-DEF_CMD(addd)
+DEF_CMD(nop, 1)
+DEF_CMD(syscall, 1)
+DEF_CMD(pushmr, 1)
+DEF_CMD(pushq, 3)
+DEF_CMD(pushr, 2)
+DEF_CMD(pushm, 1)
+DEF_CMD(popmr, 1)
+DEF_CMD(popr, 2)
+DEF_CMD(popm, 1)
+DEF_CMD(add, 1)
+DEF_CMD(sub, 1)
+DEF_CMD(mul, 1)
+DEF_CMD(div, 1)
+DEF_CMD(jmp, 4)
